fa39d1cc09f8461b26cd9411c334d2ccfa385ac7
brossetti1 <brossetti1@gmail>
2015-01-27 18:40:41 -0500

    reigrated db, adjusted functions, github api works on this version

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -15,41 +15,22 @@ class Github
   base_uri 'https://api.github.com'
   basic_auth ENV['GITHUB_USER'], ENV['GITHUB_PASS']
 
-  def get_followers(user = 'redline6561')
-    resp = self.class.get("/users/#{user}/followers")
+#user "/followers" or "/following", read Github API docs for endpoint references.
+  def get_users(user, follow_type)
+    resp = self.class.get("/users/#{user}#{follow_type}")
     json = JSON.parse(resp.body)
-    user_followers = json.shuffle[0..20]
-    followers_array = []
-    user_followers.each do |user|
-      followers_array << get_user(user['login'])
+    binding.pry
+    users = json.shuffle[0..20]
+    users_array = []
+    users.each do |user|
+      users_array << get_user(user['login'])
     end
-    followers_array
+    users_array
   end
 
-  def get_following(user = 'redline6561')
-    resp = self.class.get("/users/#{user}/following")
-    users_following = resp.shuffle[0..20]
-    following_array = []
-    users_following.each do |user|
-      following_array << get_user(user['login'])
-    end
-    following_array
-  end
-
-  #def get_users(user = 'redline6561', follow_type = '')
-  #  resp = self.class.get("/users/#{user}/#{follow_type}")
-  #  users_following = resp.shuffle[0..20]
-  #  users_array = []
-  #  users_array.each do |user|
-  #    users_array << get_user(user['login'])
-  #  end
-  #  users_array
-  #end
-
-  def get_user(user = 'redline6561', opts={})
-    opts.merge!({:basic_auth => @auth})
+  def get_user(user)
     hsh = Hash.new(0)
-    resp = self.class.get("/users/#{user}", opts)
+    resp = self.class.get("/users/#{user}")
     json = JSON.parse(resp.body)
     hsh[:login] =         json['login']
     hsh[:name] =          json['name']
@@ -60,11 +41,11 @@ class Github
     hsh
   end
 
-  def push_assoc_users_to_a(user = 'redline6561')
+  def push_assoc_users_to_a(user)
     users_array = []
     current_user = get_user(user)
-    user_following = get_following(user)
-    user_followers = get_followers(user)
+    user_following = get_users(user,'/following')
+    user_followers = get_users(user,'/followers')
     users_array.push(current_user, user_following, user_followers).flatten!
     users_array
   end
@@ -76,26 +57,17 @@ class Github
 
 end
 
-binding.pry
-
-github = Github.new()
-users = github.push_assoc_users_to_a('redline6561')#.flatten!
-
-
-users.each do |user|
-  CheepCreep::Github.create(login:        user[:login],
-                            name:         user[:name],
-                            blog:         user[:blog],
-                            public_repos: user[:public_repos],
-                            followers:    user[:followers],
-                            following:    user[:following])
+def add_users_to_db(user)
+  github = Github.new()
+  users = github.push_assoc_users_to_a(user)
+  users.each do |user|
+    CheepCreep::GithubUser.create(user)
+  end
 end
 
-users.each do |user|
-  CheepCreep::Github.create(user)
-end
 
-#github_username: testapifun password: Ironyard
+
+#github_username: apitestfun password: ironyard1
 #get, change, add, or delete gists
 
 
